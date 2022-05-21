8fc015538dd7d082e5e541a21d366d39e427410a
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-22 01:57:27 +0600

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#204047",
+        
+"secondary": "#fab915",
+        
+"accent": "#37CDBE",
+        
+"neutral": "#3D4451",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#3ABFF8",
+        
+"success": "#36D399",
+        
+"warning": "#FBBD23",
+        
+"error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
