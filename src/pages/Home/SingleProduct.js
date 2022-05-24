71de11a587c5ca3e057360adef99d64435960169
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-24 14:35:46 +0600

    added modal disabled

--- src/pages/Home/SingleProduct.js
+++ src/pages/Home/SingleProduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
@@ -6,7 +6,9 @@ import { useForm } from "react-hook-form";
 import { toast } from 'react-toastify';
 
 const SingleProduct = () => {
-    const [products, setProducts] = useState(true)
+    const inputRef = useRef('')
+    const quantity = inputRef.current.value
+   
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [user] = useAuthState(auth)
     const { id } = useParams()
@@ -44,8 +46,7 @@ const SingleProduct = () => {
         })
             .then(res => res.json())
             .then(data => {
-                if (data) {
-                    console.log(data)
+                if (data) {                   
                     toast.success("Your purshes successfully ")
                 } else {
                     toast.error("Your purshes not successfully")
@@ -70,12 +71,12 @@ const SingleProduct = () => {
                         <p className='text-2xl py-2' > <b> <span className='text-primary'>Per Unite  Price : </span> </b> $ {perUnitPrice}</p>
 
                         <label for="parshes-modal" class="btn btn-secondary text-white modal-button">Purchase</label>
-                        <input type="checkbox" id="parshes-modal" class="modal-toggle" />
+                        <input type="checkbox" id="parshes-modal" class="modal-toggle  z-40" />
                         <div class="modal z-40 " style={{zIndex:"1"}}>
-                            <div class="modal-box bg-info">
+                            <div class="modal-box bg-info  z-40">
                                 <label for="parshes-modal" class="btn btn-secondary btn-sm btn-circle absolute right-2 top-2">✕</label>
                                 <h3 className='text-center text-secondary my-2 text-2xl font-bold'>ORDAR NOW</h3>
-                                <h2 className='text-xl text-white '><b> <span className=' text-white'>Name : </span> </b> {name}</h2>
+                                <h2 className='text-xl text-white '><b> <span className=' text-white'>Name : </span> </b> {name} </h2>
                                 <p className='text-xl  text-white '><b> <span className=' text-white'>Minimun Quntity : </span> </b>{minimunQuantity}</p>
                                 <p className='text-xl  text-white'><b> <span className=' text-white'>Maximum Quntity : </span></b>{maximumQuantity}</p>
                                 <p className='text-xl  text-white' > <b> <span className=' text-white'>Per Unite  Price : </span> </b> $ {perUnitPrice}</p>
@@ -83,7 +84,7 @@ const SingleProduct = () => {
                                     <input type='text' class="input input-bordered input-info w-full max-w-xs my-2" value={user.displayName} {...register("name")} />
                                     <input type='email' class="input input-bordered input-info w-full max-w-xs my-2" value={user.email} {...register("email")} />
                                     <label className='label'>
-                                        <input class="input input-bordered input-info w-full max-w-xs my-2" type="text"  {...register("quantity", { min: minimunQuantity, max: maximumQuantity })} placeholder="Add quantiry" />
+                                        <input ref={inputRef} class="input input-bordered input-info w-full max-w-xs my-2" type="text"  {...register("quantity", { min: minimunQuantity, max: maximumQuantity })} placeholder="Add quantiry" />
                                         <label class="label ">
 
 
@@ -95,7 +96,7 @@ const SingleProduct = () => {
                                         <span className='text-red-500 font-bold'>{errors.quantity?.type === 'min' && "Minimum quaity so low to our products"}</span>
                                         <span className='text-red-500 font-bold'>{errors.quantity?.type === 'max' && "Maximum quaity hight to our products"}</span>
                                     </p>
-                                    <input for="parshes-modal" className='btn btn-secondary px-10 text-white' type='submit' value="Submit" />
+                                    <input for="parshes-modal" disabled = {errors.quantity?.type === 'min' ||  errors.quantity?.type === 'max'} className='btn btn-secondary px-10 text-white' type='submit' value="Submit" />
 
                                 </form>
 
