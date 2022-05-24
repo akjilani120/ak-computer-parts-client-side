fff62e176deb3a0a646a60fc578f6e6c6e6a3ef5
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-25 01:50:55 +0600

    added my orders data

--- src/pages/Dashboard/MyOrders.js
+++ src/pages/Dashboard/MyOrders.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading'
 const MyOrders = () => {
+    const [user] = useAuthState(auth)
+    const email = user.email
+    const {data , isLoading} = useQuery("order" , () => fetch(`http://localhost:5000/orders?email=${email}` ,{
+        method: "GET",
+        headers: {
+            "authorization": `Bearer ${localStorage.getItem("accessToken")}`
+        },
+    })
+    .then( res => res.json())
+    )
+    if( isLoading){
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1>MY orders page</h1>
+            <h1 className='text-5xl font-bold text-info'>MY  Purshes Product {console.log(" I am my favourite orders" , data)} </h1>
         </div>
     );
 };
