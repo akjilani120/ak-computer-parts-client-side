666d2a208b4b7a4c858ec47486dbdd7b44c00f81
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-26 03:03:15 +0600

    payment feature added

---
+++ src/pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,98 @@
+
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState , useEffect} from 'react';
+
+const CheckoutForm = ({data}) => {
+    const {price , userName , userEmail , productName} =  data
+   
+    const stripe = useStripe();
+    const elements = useElements();
+    const [payError , setPayError] = useState('')
+    const [success , setSuccess] = useState('')
+    const [transId , setTransId] = useState('')
+    const [clientSecret , setClientSecret] = useState("")
+    useEffect(() =>{
+        fetch("http://localhost:5000/create-payment-intent" ,{
+            method:"POST", 
+            headers: {
+                "content-type" :"application/json",
+                "authorization": `Bearer ${localStorage.getItem("accessToken")}`
+            },
+            body : JSON.stringify({price})
+        })
+        .then(res => res.json())
+        .then( data => {
+           if(data?.clientSecret){
+               setClientSecret(data.clientSecret)
+           }
+        })
+    } ,[price])
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        if (!stripe || !elements) {
+            return;
+        }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
+            return;
+        }
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type:"card", 
+            card,
+        })
+       
+            setPayError(error?.message || "")
+            setSuccess("")
+            const {paymentIntent, error: intError} = await stripe.confirmCardPayment(
+                clientSecret,
+                {
+                  payment_method: {
+                    card: card,
+                    billing_details: {
+                      name: userName,
+                      email: userEmail
+                    },
+                  },
+                },
+              );
+
+            if(intError){
+                setPayError(intError?.message)
+            }else{
+                setPayError("")                
+                setSuccess("Congratulation !! Your payment is success")
+                
+            }
+            setTransId(paymentIntent)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn btn-success text-left my-5 px-10 text-white' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            <p className='text-red-500 font-bold'>{payError}</p>
+            <p className='text-green-500 font-bold'>{success}</p>
+           { success && <p className='text-secondary font-bold'>Your Transmission Id:  {transId}</p>}
+        </div>
+    );
+};
+
+export default CheckoutForm;
--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,8 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51L1s3BBYtY7LA3umHtbhlOOjy24ztEYhVE6T9fYxHUrfRToeoghTrYQ7I7IgpHq9tr0dFVTy1bpGtFUBjDxPXdnF00j1CYjeD2');
 const Payment = () => {
     const { payId } = useParams()
     const url = `http://localhost:5000/orders/${payId}`
@@ -12,12 +16,12 @@ const Payment = () => {
             "authorization": `Bearer ${localStorage.getItem("accessToken")}`
         },
     })
-    .then( res => res.json())
+        .then(res => res.json())
     )
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
-     
+
     return (
         <div className='flex justify-center items-center'>
             <div>
@@ -26,15 +30,14 @@ const Payment = () => {
                         <p className='font-bold text-3xl'>Your Purshes</p>
                         <h2 class="text-secondary font-bold text-2xl">Hellow {data.userName}</h2>
                         <p className='font-bold text-xl'>Pay for {data.productName}</p>
-                        <p className='font-bold'>Product Price : {data.price}</p>                        
+                        <p className='font-bold'>Product Price : {data.price}</p>
                     </div>
                 </div>
                 <div class="card w-96 bg-base-100 shadow-xl mt-7">
-                    <div class="card-body text-center bg-info text-white">
-                        <p className='font-bold text-3xl'>Your Purshes</p>
-                        <h2 class="text-secondary font-bold text-2xl">Hellow {data.userName}</h2>
-                        <p className='font-bold text-xl'>Pay for {data.productName}</p>
-                        <p className='font-bold'>Product Price : {data.price}</p>                        
+                    <div class="card-body text-center ">
+                        <Elements stripe={stripePromise}>
+                            <CheckoutForm  data ={data}/>
+                        </Elements>
                     </div>
                 </div>
             </div>
